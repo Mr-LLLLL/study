0e0b908d5adc72e78b2e1be3e766126039280d13
zhaogang <zhaogang@dustess.com>
2023-06-29 00:29:49 +0800

    updates

--- rust/hello_cargo/src/main.rs
+++ rust/hello_cargo/src/main.rs
@@ -105,6 +105,29 @@ fn hashmap_practice() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{score}");
+
+    for (key, value) in &scores {
+        println!("{key}, {value}");
+    }
+
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map)
 }
 
 fn main() {
