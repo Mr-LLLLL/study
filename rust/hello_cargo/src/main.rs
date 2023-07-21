8e95c497764e8500d5874201bea981cd5b4ed166
zhaogang <zhaogang@dustess.com>
2023-07-21 16:10:44 +0800

    updates

--- rust/hello_cargo/src/main.rs
+++ rust/hello_cargo/src/main.rs
@@ -5,7 +5,8 @@ use std::fmt::{Display, Result};
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
 use std::io::{ErrorKind, Result as IoResult};
-use std::{collections::*, panic, result, vec};
+use std::time::Duration;
+use std::{collections::*, panic, result, thread, vec};
 
 fn deliver_order() {}
 
@@ -535,8 +536,98 @@ fn livecycle_practice() {
     println!("The longest string is {res1}");
 }
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        num
+    };
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
+        }
+    }
+}
+
+fn closures_practice() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    let mut mut_list = vec![1, 2, 3, 4];
+    let mut borrows_mutably = || mut_list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", mut_list);
+}
+
 fn main() {
-    livecycle_practice();
+    closures_practice();
 }
 
 fn add_fancy_hat() {}
