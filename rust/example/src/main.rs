8c7b09ef7ee3c0c816534377fd061135a0b566d3
zhaogang <zhaogang@dustess.com>
2024-01-19 16:22:27 +0800

    updates

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -115,6 +115,24 @@ fn practise_format() {
     }
 }
 
+#[allow(dead_code)]
+enum ColorEnum {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+enum FooEnum {
+    Bar,
+    Baz,
+    Qux(u32),
+}
+
 fn practice_procession() {
     let n = 5;
     if n < 0 {
@@ -209,6 +227,154 @@ fn practice_procession() {
         }
     }
     println!("names {:?}", names);
+
+    let number = 19;
+    println!("Tell me about {}", number);
+    match number {
+        1 => println!("one!"),
+        2 | 3 | 5 | 7 | 11 => println!("This is prime!"),
+        13..=19 => println!("A teen"),
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    };
+    println!("{} -> {}", boolean, binary);
+
+    let triple = (0, -2, 3);
+    println!("Tell me about {:?}", triple);
+    match triple {
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..) => println!("First is `1` and the rest doesn't matter"),
+        _ => println!("It doesn't matter what they are"),
+    };
+
+    let color = ColorEnum::RGB(122, 17, 40);
+
+    println!("What color is is?");
+    match color {
+        ColorEnum::Red => println!("The color is Red!"),
+        ColorEnum::RGB(r, g, b) => println!("Red: {}, green: {}, blue: {}", r, g, b),
+        ColorEnum::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key(black): {}",
+            c, m, y, k
+        ),
+        _ => println!("Others"),
+    };
+
+    let reference = &4;
+    match reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    match *reference {
+        val => println!("Got a value via deferencing: {:?}", val),
+    }
+
+    let _not_a_reference = 3;
+    let value = 5;
+    let mut mut_value = 6;
+
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m)
+        }
+    }
+
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo = Foo { x: (1, 2), y: 3 };
+    let Foo { x: (a, b), y } = foo;
+    println!("a = {}, b = {}, y = {}", a, b, y);
+
+    let Foo { y: i, x: j } = foo;
+    println!("i = {:?}, j = {:?}", i, j);
+
+    let Foo { y, .. } = foo;
+    println!("y = {}", y);
+
+    let pair = (2, -2);
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("There are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+
+    let age = || 15;
+    match age() {
+        0 => println!("I haven't celebrated my first birthday yet"),
+        n @ 1..=12 => println!("I'm a child of age {:?}", n),
+        n @ 13..=19 => println!("I'm a teen of age {:?}", n),
+        n => println!("I'm an old person of age {:?}", n),
+    }
+
+    let some_numer = || Some(42);
+    match some_numer() {
+        Some(n @ 42) => println!("The Answer: {}!", n),
+        Some(n) => println!("Not interesting...{}", n),
+        _ => (),
+    }
+
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Matched {:?}", i);
+    }
+
+    if let Some(i) = letter {
+        println!("Matched {:?}", i);
+    } else {
+        println!("Didn't match a number. Let's go while a letter!");
+    }
+
+    let i_like_letters = false;
+    if let Some(i) = emoticon {
+        println!("Matched {:?}", i);
+    } else if i_like_letters {
+        println!("Didn't match a number. Let's go with a letter!");
+    } else {
+        println!("I don't like letter. Let's go with an emoticon :)!");
+    }
+
+    let a = FooEnum::Bar;
+    let b = FooEnum::Baz;
+    let c = FooEnum::Qux(100);
+
+    if let FooEnum::Bar = a {
+        println!("a is foobar");
+    }
+    if let FooEnum::Bar = b {
+        println!("b is foobar");
+    }
+    if let FooEnum::Qux(value) = c {
+        println!("c is {}", value);
+    }
+
+    let mut optional = Some(0);
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+    }
 }
 
 fn main() {
