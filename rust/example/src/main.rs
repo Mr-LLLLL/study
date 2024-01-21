3b89464f10a99ca8450d31a31299dc054a791867
zhaogang <zhaogang@dustess.com>
2024-01-21 23:48:52 +0800

    updates

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -470,6 +470,58 @@ fn practice_function() {
     // 报错！前面的 `destroy` 调用 “消耗了” `pair`
     // pair.destroy();
     // 试一试 ^ 将此行注释去掉
+
+    fn function(i: i32) -> i32 {
+        i + 1
+    }
+
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred = |i| i + 1;
+    let i = 1;
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+    let color = String::from("green");
+    let print = || println!("`color`: {}", color);
+    print();
+    let _reborrow = &color;
+    print();
+    let _color_moved = color;
+
+    let mut count = 0;
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+    inc();
+    inc();
+    let _count_reborrowed = &mut count;
+
+    let movable = Box::new(3);
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        std::mem::drop(movable);
+    };
+    consume();
+    // consume();
+
+    // `Vec` 在语义上是不可复制的。
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+    //println!("There're {} elements in vec", haystack.len());
+    // ^ 取消上面一行的注释将导致编译时错误，因为借用检查不允许在变量被移动走
+    // 之后继续使用它。
+
+    // 在闭包的签名中删除 `move` 会导致闭包以不可变方式借用 `haystack`，因此之后
+    // `haystack` 仍然可用，取消上面的注释也不会导致错误。
 }
 
 fn main() {
