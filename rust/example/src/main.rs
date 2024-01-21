2ae84c1b3b5ec67273465f68f321a3c53fb6ecd4
zhaogang <zhaogang@dustess.com>
2024-01-21 14:51:16 +0800

    updates

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -133,6 +133,7 @@ enum FooEnum {
     Qux(u32),
 }
 
+#[allow(dead_code)]
 fn practice_procession() {
     let n = 5;
     if n < 0 {
@@ -377,6 +378,100 @@ fn practice_procession() {
     }
 }
 
+#[allow(dead_code)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+#[allow(dead_code)]
+impl Point {
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+}
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+#[allow(dead_code)]
+impl Rectangle {
+    fn area(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+
+    fn perimeter(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+    }
+
+    fn translate(&mut self, x: f64, y: f64) {
+        self.p1.x += x;
+        self.p2.x += x;
+
+        self.p1.y += y;
+        self.p2.y += y;
+    }
+}
+
+#[allow(dead_code)]
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+    #[allow(dead_code)]
+    fn destroy(self) {
+        let Pair(first, second) = self;
+
+        println!("Destroying Pair({}, {})", first, second);
+    }
+}
+
+#[allow(dead_code)]
+fn practice_function() {
+    let rectangle = Rectangle {
+        // 静态方法使用双冒号调用
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    // 实例方法通过点运算符来调用
+    // 注意第一个参数 `&self` 是隐式传递的，亦即：
+    // `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    // 报错！ `rectangle` 是不可变的，但这方法需要一个可变对象
+    //rectangle.translate(1.0, 0.0);
+    // 试一试 ^ 去掉此行的注释
+
+    // 正常运行！可变对象可以调用可变方法
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+
+    // 报错！前面的 `destroy` 调用 “消耗了” `pair`
+    // pair.destroy();
+    // 试一试 ^ 将此行注释去掉
+}
+
 fn main() {
-    practice_procession();
+    practice_function();
 }
