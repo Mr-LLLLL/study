6cc0dbd22afac2268ab9a88056f4ab3916aae46e
zhaogang <zhaogang@dustess.com>
2024-02-03 17:28:54 +0800

    updates

--- rust/example/src/main.rs
+++ rust/example/src/main.rs
@@ -385,6 +385,7 @@ fn prictise_procession() {
 }
 
 #[allow(dead_code)]
+#[derive(Clone, Copy)]
 struct Point {
     x: f64,
     y: f64,
@@ -1020,6 +1021,212 @@ fn practise_generic() {
     println!("one meter + one merter = {:?} mm", two_meters.0);
 }
 
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
+fn create_box() {
+    let _box1 = Box::new(3i32);
+}
+
+fn destroy_box(c: Box<i32>) {
+    println!("Destroying a box that contains {}", c);
+}
+
+fn eat_box_i32(boxed_i32: Box<i32>) {
+    println!("Destroying box that contains {}", boxed_i32);
+}
+
+fn borrow_i32(borrowed_i32: &i32) {
+    println!("This int is: {}", borrowed_i32);
+}
+
+#[derive(Clone, Copy)]
+struct Book {
+    author: &'static str,
+    title: &'static str,
+    year: u32,
+}
+
+#[derive(Clone, Copy)]
+struct Point1 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+fn borrow_book(book: &Book) {
+    println!(
+        "I immutably borrowed {} - {} edition",
+        book.title, book.year
+    );
+}
+
+fn new_edition(book: &mut Book) {
+    book.year = 2014;
+    println!("I mutably borrowed {} - {} edition", book.title, book.year);
+}
+
+fn practise_lifetime() {
+    let _box2 = Box::new(5i32);
+
+    {
+        let _box3 = Box::new(4i32);
+    }
+
+    for _ in 0u32..1_000 {
+        create_box();
+    }
+
+    let _x = ToDrop;
+    let mut _y = _x;
+    println!("Made a ToDrop!");
+
+    let x = 5u32;
+    let y = x;
+
+    println!("{}, {}", x, y);
+
+    let a = Box::new(5i32);
+    println!("a contains: {}", a);
+
+    let b = a;
+
+    destroy_box(b);
+
+    let immutable_box = Box::new(5u32);
+    println!("ummutable_box contains: {}", immutable_box);
+
+    let mut mutable_box = immutable_box;
+    println!("mutable_box contains: {}", mutable_box);
+
+    *mutable_box = 4;
+    println!("mutable_box now contains: {}", mutable_box);
+
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: u8,
+    }
+
+    let person = Person {
+        name: String::from("Alice"),
+        age: 20,
+    };
+
+    let Person { name, ref age } = person;
+    println!("The person's age is {}", age);
+    println!("The person's name is {}", name);
+
+    // println!("The person's age from person struct is {}", person.name);
+    println!("The person's age from person struct is {}", person.age);
+
+    let boxed_i32 = Box::new(5_i32);
+    let stacked_i32 = 6_i32;
+
+    borrow_i32(&boxed_i32);
+    borrow_i32(&stacked_i32);
+
+    {
+        let _ref_to_i32: &i32 = &boxed_i32;
+
+        // eat_box_i32(boxed_i32);
+
+        borrow_i32(_ref_to_i32);
+    }
+
+    eat_box_i32(boxed_i32);
+
+    let immutablebook = Book {
+        author: "Douglas Hofstander",
+        title: "Godle, Eschar, Bach",
+        year: 1979,
+    };
+
+    let mut mutabook = immutablebook;
+
+    borrow_book(&immutablebook);
+    borrow_book(&mutabook);
+    new_edition(&mut mutabook);
+
+    // new_edition(&mut immutablebook);
+
+    let mut point = Point1 { x: 0, y: 0, z: 0 };
+
+    let borrowed_point = &point;
+    let another_borrow = &point;
+
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        borrowed_point.x, another_borrow.y, point.z
+    );
+
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        borrowed_point.x, another_borrow.y, point.z
+    );
+
+    let mutable_borrow = &mut point;
+
+    mutable_borrow.x = 5;
+    mutable_borrow.y = 2;
+    mutable_borrow.z = 1;
+
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        mutable_borrow.x, mutable_borrow.y, mutable_borrow.z
+    );
+
+    let new_borrowed_point = &point;
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        new_borrowed_point.x, new_borrowed_point.y, new_borrowed_point.z
+    );
+
+    let c = 'Q';
+    let ref ref_c1 = c;
+    let ref_c2 = &c;
+    println!("ref_c1 equal ref_c2: {}", *ref_c1 == *ref_c2);
+
+    let point = Point { x: 0.0, y: 0.0 };
+
+    let _copy_of_x = {
+        let Point {
+            x: ref ref_to_x,
+            y: _,
+        } = point;
+
+        *ref_to_x
+    };
+
+    let mut mutable_point = point;
+    {
+        let Point {
+            x: _,
+            y: ref mut mut_ref_to_y,
+        } = mutable_point;
+
+        *mut_ref_to_y = 1.0;
+    }
+    println!("point is ({}, {})", point.x, point.y);
+    println!(
+        "mutable_point is ({}, {})",
+        mutable_point.x, mutable_point.y
+    );
+
+    let mut mutable_tuple = (Box::new(5u32), 3u32);
+    {
+        let (_, ref mut last) = mutable_tuple;
+        *last = 2u32;
+    }
+
+    println!("tuple is {:?}", mutable_tuple);
+}
+
 fn main() {
-    practise_generic();
+    practise_lifetime();
 }
