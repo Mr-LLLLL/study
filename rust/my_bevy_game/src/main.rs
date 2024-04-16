10bce3260c400d9d4635ec5289bb7a906bbb3ed7
zhaogang <zhaogang@dustess.com>
2024-04-16 22:22:17 +0800

    updates

--- rust/my_bevy_game/src/main.rs
+++ rust/my_bevy_game/src/main.rs
@@ -6,7 +6,7 @@ impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+            .add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
@@ -16,17 +16,10 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn hello_world() {
-    println!("Hello, world!");
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
@@ -38,14 +31,19 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
-            println!("Hello, {}!", name.0);
+            println!("hello {}!", name.0);
         }
     }
 }
 
 fn main() {
-    App::new().add_plugins(HelloPlugin).run();
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
