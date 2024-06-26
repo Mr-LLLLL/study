532b38b8fbb2b38f2e31db67720cd3508947732a
zhaogang <zhaogang@dustess.com>
2024-06-26 16:32:21 +0800

    update

--- rust/my_bevy_game/src/main.rs
+++ rust/my_bevy_game/src/main.rs
@@ -7,5 +7,5 @@ mod two_dimension;
 mod window;
 
 fn main() {
-    two_dimension::sprite_slice::run();
+    two_dimension::mesh2d_manual::run();
 }
--- rust/my_bevy_game/src/two_dimension/mod.rs
+++ rust/my_bevy_game/src/two_dimension/mod.rs
@@ -3,6 +3,7 @@ pub mod bounding;
 pub mod custom_gltf_vertex_attribute;
 pub mod gizmos;
 pub mod mesh2d;
+pub mod mesh2d_manual;
 pub mod mesh2d_vertex_color_texture;
 pub mod move_sprite;
 pub mod pixel_grid_snap;
