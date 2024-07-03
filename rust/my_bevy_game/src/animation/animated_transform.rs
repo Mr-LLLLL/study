caa986ee1d471e2cb8b57e4d3662ee69285a3613
zhaogang <zhaogang@dustess.com>
2024-07-03 10:18:06 +0800

    update

--- rust/my_bevy_game/src/animation/animated_transform.rs
+++ rust/my_bevy_game/src/animation/animated_transform.rs
@@ -1,14 +1,18 @@
-use bevy::prelude::*;
+//! Create and play an animation defined by code that operates on the [`Transform`] component.
+
 use std::f32::consts::PI;
+
+use bevy::prelude::*;
 
 pub fn run() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(AmbientLight {
             color: Color::WHITE,
-            brightness: 150.,
+            brightness: 150.0,
         })
-        .add_systems(Startup, (setup, bevy::window::close_on_esc))
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
@@ -18,49 +22,58 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut animations: ResMut<Assets<AnimationClip>>,
 ) {
+    // Camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2., 2.5, 5.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
+    // Light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 500_000.0,
             ..default()
         },
-        transform: Transform::from_xyz(0., 2.5, 0.),
+        transform: Transform::from_xyz(0.0, 2.5, 0.0),
         ..default()
     });
 
+    // Let's use the `Name` component to target entities. We can use anything we
+    // like, but names are convenient.
     let planet = Name::new("planet");
     let orbit_controller = Name::new("orbit_controller");
     let satellite = Name::new("satellite");
 
+    // Creating the animation
     let mut animation = AnimationClip::default();
-
+    // A curve can modify a single part of a transform, here the translation
     animation.add_curve_to_path(
         EntityPath {
             parts: vec![planet.clone()],
         },
         VariableCurve {
-            keyframe_timestamps: vec![0., 1., 2., 3., 4.],
+            keyframe_timestamps: vec![0.0, 1.0, 2.0, 3.0, 4.0],
             keyframes: Keyframes::Translation(vec![
-                Vec3::new(1., 0., 1.),
-                Vec3::new(-1., 0., 1.),
-                Vec3::new(-1., 0., -1.),
-                Vec3::new(1., 0., -1.),
-                Vec3::new(1., 0., 1.),
+                Vec3::new(1.0, 0.0, 1.0),
+                Vec3::new(-1.0, 0.0, 1.0),
+                Vec3::new(-1.0, 0.0, -1.0),
+                Vec3::new(1.0, 0.0, -1.0),
+                // in case seamless looping is wanted, the last keyframe should
+                // be the same as the first one
+                Vec3::new(1.0, 0.0, 1.0),
             ]),
             interpolation: Interpolation::Linear,
         },
     );
-
+    // Or it can modify the rotation of the transform.
+    // To find the entity to modify, the hierarchy will be traversed looking for
+    // an entity with the right name at each level
     animation.add_curve_to_path(
         EntityPath {
             parts: vec![planet.clone(), orbit_controller.clone()],
         },
         VariableCurve {
-            keyframe_timestamps: vec![0., 1., 2., 3., 4.],
+            keyframe_timestamps: vec![0.0, 1.0, 2.0, 3.0, 4.0],
             keyframes: Keyframes::Rotation(vec![
                 Quat::IDENTITY,
                 Quat::from_axis_angle(Vec3::Y, PI / 2.),
@@ -71,13 +84,15 @@ fn setup(
             interpolation: Interpolation::Linear,
         },
     );
-
+    // If a curve in an animation is shorter than the other, it will not repeat
+    // until all other curves are finished. In that case, another animation should
+    // be created for each part that would have a different duration / period
     animation.add_curve_to_path(
         EntityPath {
             parts: vec![planet.clone(), orbit_controller.clone(), satellite.clone()],
         },
         VariableCurve {
-            keyframe_timestamps: vec![0., 0.5, 1., 1.5, 2., 2.5, 3., 3.5, 4.],
+            keyframe_timestamps: vec![0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0],
             keyframes: Keyframes::Scale(vec![
                 Vec3::splat(0.8),
                 Vec3::splat(1.2),
@@ -92,13 +107,13 @@ fn setup(
             interpolation: Interpolation::Linear,
         },
     );
-
+    // There can be more than one curve targeting the same entity path
     animation.add_curve_to_path(
         EntityPath {
             parts: vec![planet.clone(), orbit_controller.clone(), satellite.clone()],
         },
         VariableCurve {
-            keyframe_timestamps: vec![0., 1., 2., 3., 4.],
+            keyframe_timestamps: vec![0.0, 1.0, 2.0, 3.0, 4.0],
             keyframes: Keyframes::Rotation(vec![
                 Quat::IDENTITY,
                 Quat::from_axis_angle(Vec3::Y, PI / 2.),
@@ -110,9 +125,12 @@ fn setup(
         },
     );
 
+    // Create the animation player, and set it to repeat
     let mut player = AnimationPlayer::default();
     player.play(animations.add(animation)).repeat();
 
+    // Create the scene that will be animated
+    // First entity is the planet
     commands
         .spawn((
             PbrBundle {
@@ -120,21 +138,29 @@ fn setup(
                 material: materials.add(Color::rgb(0.8, 0.7, 0.6)),
                 ..default()
             },
+            // Add the Name component, and the animation player
             planet,
             player,
         ))
         .with_children(|p| {
-            p.spawn((SpatialBundle::INHERITED_IDENTITY, orbit_controller))
-                .with_children(|p| {
-                    p.spawn((
-                        PbrBundle {
-                            transform: Transform::from_xyz(1.5, 0., 0.),
-                            mesh: meshes.add(Cuboid::new(0.5, 0.5, 0.5)),
-                            material: materials.add(Color::rgb(0.3, 0.9, 0.3)),
-                            ..default()
-                        },
-                        satellite,
-                    ));
-                });
+            // This entity is just used for animation, but doesn't display anything
+            p.spawn((
+                SpatialBundle::INHERITED_IDENTITY,
+                // Add the Name component
+                orbit_controller,
+            ))
+            .with_children(|p| {
+                // The satellite, placed at a distance of the planet
+                p.spawn((
+                    PbrBundle {
+                        transform: Transform::from_xyz(1.5, 0.0, 0.0),
+                        mesh: meshes.add(Cuboid::new(0.5, 0.5, 0.5)),
+                        material: materials.add(Color::rgb(0.3, 0.9, 0.3)),
+                        ..default()
+                    },
+                    // Add the Name component
+                    satellite,
+                ));
+            });
         });
 }
--- rust/my_bevy_game/src/main.rs
+++ rust/my_bevy_game/src/main.rs
@@ -8,9 +8,5 @@ mod two_dimension;
 mod window;
 
 fn main() {
-<<<<<<< HEAD
-    animation::animated_transform::run();
-=======
     animation::morph_targets::run();
->>>>>>> 8f360b4b29d7714d63909efef9ac496b9dbda7b1
 }
