1e1bdd3868f1ba2a0d82693e99e9058914347713
zhaogang <zhaogang@dustess.com>
2024-07-27 10:16:39 +0800

    update

--- rust/tokio/src/example/mod.rs
+++ rust/tokio/src/example/mod.rs
@@ -5,4 +5,8 @@ pub mod custom_executor_tokio_context;
 pub mod dump;
 pub mod echo;
 pub mod hello_world;
+<<<<<<< HEAD
 pub mod proxy;
+=======
+pub mod print_each_packet;
+>>>>>>> 07bd7ad (update)
--- rust/tokio/src/main.rs
+++ rust/tokio/src/main.rs
@@ -1,4 +1,6 @@
 #![allow(dead_code)]
+
+use std::{thread, time::Duration};
 mod example;
 
 pub fn main() {
@@ -9,4 +11,57 @@ pub fn main() {
     // example::custom_executor_tokio_context::run();
     // example::dump::run();
     // example::proxy::run();
+    // example::print_each_packet::run();
+
+    // tokio_main();
+    // tokio_spawn();
+    tokio_work_may_dropped();
+}
+
+fn tokio_work_may_dropped() {
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
+        tokio::spawn(process());
+    })
+}
+
+#[tokio::main]
+async fn tokio_spawn() {
+    // here running process
+    let handle = tokio::spawn(process());
+
+    thread::sleep(Duration::from_secs(1));
+
+    println!("hello");
+
+    thread::sleep(Duration::from_secs(1));
+
+    let out = handle.await.unwrap();
+    println!("Main Finished Got {}", out);
+}
+
+fn tokio_main() {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let op = process();
+
+        thread::sleep(Duration::from_secs(1));
+
+        println!("hello");
+
+        thread::sleep(Duration::from_secs(1));
+
+        // here running process
+        op.await;
+        println!("Main finished");
+    });
+}
+
+async fn process() -> i32 {
+    println!("world");
+
+    thread::sleep(Duration::from_secs(1));
+
+    println!("coroutine finished");
+
+    return 1;
 }
